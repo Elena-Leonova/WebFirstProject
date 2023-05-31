0678ee5210abb3cf6c30ac27984a44b9f80f4b9f
Elena-Leonova <lena.postrash@gmail.com>
2023-05-31 19:54:52 +0300

    MyProject_Web2

---
+++ WebFirstProject/src/test/java/MyProject_Web2.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MyProject_Web2 {
+    WebDriver webdriver;
+    @Test
+    public void openAndInput(){
+        webdriver = new ChromeDriver();
+        webdriver.navigate().to("https://ya.ru/");
+        webdriver.findElement(By.name("text")).sendKeys("Adel"+ Keys.ENTER);
+    }
+
+}
